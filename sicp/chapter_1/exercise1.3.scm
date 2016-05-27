729e23aaa9d60a9e6456b286a457d6ecf03a6faa
rahul <srahul07@gmail.com>
2016-05-28 01:25:14 +0530

    Adding two methods for maximum calculations

--- sicp/chapter_1/exercise1.3.scm
+++ sicp/chapter_1/exercise1.3.scm
@@ -3,5 +3,13 @@
 ; Define a procedure that takes 3 numbers as arguments and returns sum of the squares of the two larger numbers
 
 ; The max method is suppose to be used
-(define (max a b)
+(define (max1 a b)
 	(if (> a b) a b))
+
+; The other solution to find max
+(define (abs a)
+	(if (< a 0) (- a) a))
+
+
+(define (max2 a b)
+	(/ (+ (+ a b) (abs (- a b))) 2))
