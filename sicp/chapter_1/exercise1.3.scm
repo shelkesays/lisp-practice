6f3657c1648ca5fbe1094a5e3e94ac9f811758da
rahul <srahul07@gmail.com>
2016-05-28 01:15:37 +0530

    adding problem statement

--- sicp/chapter_1/exercise1.3.scm
+++ sicp/chapter_1/exercise1.3.scm
@@ -1,2 +1,7 @@
+; The problem
+; 
+; Define a procedure that takes 3 numbers as arguments and returns sum of the squares of the two larger numbers
+
+; The max method is suppose to be used
 (define (max a b)
 	(if (> a b) a b))
